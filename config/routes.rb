4aa9102a3abeb93c5151c6abb5401d709fc48b06
heiho <heiho.developer@gmail.com>
2021-07-03 09:05:38 +0900

    omniauth 追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks" }
+  root 'pages#index'
+  get 'pages/show'
+  get 'pages/index'
   get 'test/starter'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "login", to: "login#index"
 end
