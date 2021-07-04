0cfca87ffdaf52ef7671d2cec05360244a29d30d
heiho <heiho.developer@gmail.com>
2021-07-04 19:15:30 +0900

    no message

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :active_record_store, :key => '_komagoma_session'
+Rails.application.config.session_store :active_record_store, :key => '_komagoma_session', :httponly => false
