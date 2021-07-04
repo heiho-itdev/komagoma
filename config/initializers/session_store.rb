38a94a3e6363e2f2fc70f04478ac68be10687897
heiho <heiho.developer@gmail.com>
2021-07-04 21:40:03 +0900

    セッションhttponly=false 削除

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :active_record_store, :key => '_komagoma_session', :httponly => false
+Rails.application.config.session_store :active_record_store, :key => '_komagoma_session'
